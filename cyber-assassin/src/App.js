ddf9b6b991d139542278685ee8375568c37d1c01
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 22:10:36 +0530

    dsds

--- cyber-assassin/src/App.js
+++ cyber-assassin/src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
-//import React,{useState} from 'react';
 import './style/style.css';
 import Home from './pages/Home/Home.jsx';
 import Footer from './pages/Footerbar/footerbar.jsx';
 import Nav  from './nav/nav.jsx';
-
 
 class App extends Component {
   render() {
