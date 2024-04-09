0afd5274e916c3dad3d941b14626a72b0b42fc85
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 21:35:29 +0530

    dss

--- cyber-assassin/src/App.js
+++ cyber-assassin/src/App.js
@@ -3,10 +3,21 @@ import './style/style.css';
 import Home from './pages/Home/Home.jsx';
 import Footer from './pages/Footerbar/footerbar.jsx';
 import Nav  from './nav/nav.jsx';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Page from './pages/test.jsx';
 class App extends Component {
   render() {
     return (
     <div>
+      <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="./pages/test.jsx" component={Page} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
      <Home />
      <Footer />
      <Nav />
---
+++ cyber-assassin/src/pages/test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page() {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Welcome to the Home page!</p>
+    </div>
+  );
+}
+
+export default Page;
