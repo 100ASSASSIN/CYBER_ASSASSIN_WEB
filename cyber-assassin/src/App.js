8d40faa53bfeb0f2d5f73d74f250a05abd626530
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 11:16:58 +0530

    dsds

--- cyber-assassin/src/App.js
+++ cyber-assassin/src/App.js
@@ -5,8 +5,8 @@ class App extends Component {
   render() {
     return (
     <div>
-    <h1>CYBER-ASSASSIN</h1> <h2>Panel</h2>
-    <div id='nav'> new</div>
+      <h1>CYBER-ASSASSIN</h1>
+     
      </div>
     );
   }
