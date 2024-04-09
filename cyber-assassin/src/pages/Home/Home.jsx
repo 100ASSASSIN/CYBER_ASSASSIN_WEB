cafd3c92f583431ea68491248ee069be3a1747c7
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 13:03:17 +0530

    dsds

--- cyber-assassin/src/pages/Home/Home.jsx
+++ cyber-assassin/src/pages/Home/Home.jsx
@@ -4,41 +4,37 @@ const Home = () => {
     return(
         <div id="Home">
         <div id="Home2">
-        # CYBER_ASSASSIN_WEB
-# CYBER_ASSASSIN_TOOLS_CODE_DOCX 
-
-# list of various types of cyber attacks:
-
-1. Phishing attacks
-2. Malware attacks (viruses, worms, trojans, ransomware, spyware, adware)
-3. Denial of Service (DoS) attacks
-4. Distributed Denial of Service (DDoS) attacks
-5. Man-in-the-Middle (MitM) attacks
-6. SQL injection attacks
-7. Cross-Site Scripting (XSS) attacks
-8. Brute force attacks
-9. Social engineering attacks
-10. Insider attacks
-11. Zero-day exploits
-12. Spoofing attacks
-13. DNS Spoofing attacks
-14. Session Hijacking attacks
-15. Eavesdropping attacks
-16. Password attacks (password guessing, password cracking)
-17. Clickjacking attacks
-18. Pharming attacks
-19. Watering hole attacks
-20. Drive-by download attacks
-21. Rogue software attacks
-22. Typosquatting attacks
-23. Keylogger attacks
-24. Ransomware attacks
-25. Botnet attacks
-26. Fileless attacks
-27. Advanced Persistent Threat (APT) attacks
-28. Cryptojacking attacks
-29. Supply chain attacks
-30. Credential stuffing attacks
+List of various types of cyber attacks:<br></br>
+1. Phishing attacks<br></br>
+2. Malware attacks (viruses, worms, trojans, ransomware, spyware, adware)<br></br>
+3. Denial of Service (DoS) attacks<br></br>
+4. Distributed Denial of Service (DDoS) attacks<br></br>
+5. Man-in-the-Middle (MitM) attacks<br></br>
+6. SQL injection attacks<br></br>
+7. Cross-Site Scripting (XSS) attacks<br></br>
+8. Brute force attacks<br></br>
+9. Social engineering attacks<br></br>
+10. Insider attacks<br></br>
+11. Zero-day exploits<br></br>
+12. Spoofing attacks<br></br>
+13. DNS Spoofing attacks<br></br>
+14. Session Hijacking attacks<br></br>
+15. Eavesdropping attacks<br></br>
+16. Password attacks (password guessing, password cracking)<br></br>
+17. Clickjacking attacks<br></br>
+18. Pharming attacks<br></br>
+19. Watering hole attacks<br></br>
+20. Drive-by download attacks<br></br>
+21. Rogue software attacks<br></br>
+22. Typosquatting attacks<br></br>
+23. Keylogger attacks<br></br>
+24. Ransomware attacks<br></br>
+25. Botnet attacks<br></br>
+26. Fileless attacks<br></br>
+27. Advanced Persistent Threat (APT) attacks<br></br>
+28. Cryptojacking attacks<br></br>
+29. Supply chain attacks<br></br>
+30. Credential stuffing attacks<br></br>
         </div>
         </div>
 
