44fc33faa7c71979e11b29d14b92e86cc4a8503f
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 23:15:51 +0530

    sdsds

--- cyber-assassin/src/App.js
+++ cyber-assassin/src/App.js
@@ -2,17 +2,43 @@ import React, { Component } from 'react';
 import './style/style.css';
 import Home from './pages/Home/Home.jsx';
 import Footer from './pages/Footerbar/footerbar.jsx';
-import Nav  from './nav/nav.jsx';
+import Nav from './nav/nav.jsx';
+import Page from './pages/test.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 'Home'
+    };
+  }
+
+  renderPage() {
+    const { currentPage } = this.state;
+    switch (currentPage) {
+      case 'Home':
+        return <Home />;
+      case 'Page':
+        return <Page />;
+      default:
+        return <Home />;
+    }
+  }
+
+  navigate(page) {
+    this.setState({ currentPage: page });
+  }
+
   render() {
     return (
-    <div>
-     <Home />
-     <Footer />
-     <Nav />
-     </div>
+      <div>
+        <button id='b1' onClick={() => this.navigate('Page')}>Navigate to Page</button>
+        {this.renderPage()}
+        <Footer />
+        <Nav />
+      </div>
     );
   }
 }
+
 export default App;
