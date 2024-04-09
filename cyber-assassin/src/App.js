268c578c33d1f1320c7aa74a630ae4c9e24c318e
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 20:37:16 +0530

    dss

--- cyber-assassin/src/App.js
+++ cyber-assassin/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './style/style.css';
 import Home from './pages/Home/Home.jsx';
 import Footer from './pages/Footerbar/footerbar.jsx';
-import Nav  from './nav/nav.js';
+import Nav  from './nav/nav.jsx';
 class App extends Component {
   render() {
     return (
--- cyber-assassin/src/nav/nav.jsx
+++ cyber-assassin/src/nav/nav.jsx
@@ -3,10 +3,12 @@ import './nav.css'
 
 const Nav = () => {
     return(
+        <div>
         <div id="nav">
  <h1>CYBER-ASSASSIN</h1>
       <h2>Panel</h2>
       < a href='http://45.129.86.135'><h2 id='com'>our Companey</h2></a>
+        </div>
         </div>
     );
 }
