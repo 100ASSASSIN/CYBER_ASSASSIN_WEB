9d8ff3c87bee9c77d10fd4ac60156f3748d453f4
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 11:31:19 +0530

    dsdd

--- cyber-assassin/src/App.js
+++ cyber-assassin/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './style/style.css';
-
+import Home from './pages/Home/Home.jsx';
 class App extends Component {
   render() {
     return (
     <div>
+     <Home />
       <h1>CYBER-ASSASSIN</h1>
      </div>
     );
   }
 }
-
 export default App;
---
+++ cyber-assassin/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import '../WebWeaver/WebWeaver.css'
+const Home = () => {
+    return(
+        <div id="Home">TEST</div>
+
+    );
+}
+export default Home;
