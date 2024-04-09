4ec00d5ae2cb773d59fa1afb664d0c0c07285b95
ASSASSIN <tamilthasan11@gamil.com>
2024-04-09 20:33:07 +0530

    sdsd

--- cyber-assassin/src/App.js
+++ cyber-assassin/src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import './style/style.css';
 import Home from './pages/Home/Home.jsx';
 import Footer from './pages/Footerbar/footerbar.jsx';
+import Nav  from './nav/nav.js';
 class App extends Component {
   render() {
     return (
     <div>
      <Home />
      <Footer />
-      <h1>CYBER-ASSASSIN</h1>
-      <h2>Panel</h2>
-      <h2 id='com'>our Companey</h2>
+     <Nav />
      </div>
     );
   }
---
+++ cyber-assassin/src/nav/nav.js
@@ -0,0 +1,13 @@
+import React  from "react";
+import './nav.css'
+
+const Nav = () => {
+    return(
+        <div id="nav">
+ <h1>CYBER-ASSASSIN</h1>
+      <h2>Panel</h2>
+      < a href='http://45.129.86.135'><h2 id='com'>our Companey</h2></a>
+        </div>
+    );
+}
+export default Nav;
